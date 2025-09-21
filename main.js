bae0cd932518f8aad8be0835b9579740ba9fea44
Controluser <controluser132@gmail.com>
2025-09-21 22:17:08 +0530

    search page

--- main.js
+++ main.js
@@ -29,3 +29,52 @@ scrollBtn.addEventListener('click', () => {
 
 
 const boxes = document.querySelectorAll('.ins-background')
+
+// inspiration section - auto
+const backgrounds = document.querySelectorAll('.ins-background-wrapper');
+const dinner = document.querySelector('.dinner')
+const dinBoxes = dinner.querySelectorAll('.box')
+const home = document.querySelector('.home')
+const homBoxes = home.querySelectorAll('.box')
+const indexBtn = document.querySelectorAll('.index-btn');
+const text = document.getElementById('text')
+const hexColors = ['#c28b00', '#618c7b', '#0076d3', '#407a57']
+index = 0
+
+
+function showNext() {
+
+  // remove "show"(class) from background-wrappers
+
+  //show current background-wrapper
+  backgrounds[index].classList.add('active')
+
+  // move to next background-wrapper
+  index++
+  if(index >= backgrounds.length) {
+    index = 0
+    setTimeout(removeActive, 7500)
+  }
+
+  // current = (current + 1) % backgrounds.length
+  // backgrounds[current].classList.add('show')
+  
+  // show first
+}
+showNext()
+setInterval(showNext, 6000)
+
+// setTimeout(auto, 5800)
+
+function removeActive() {
+  backgrounds.forEach(s => {
+    s.classList.remove('active')
+    s.querySelectorAll('.box').forEach(b => {
+      b.style.animation = 'none'
+      b.offsetHeight
+      b.style.animation = ''
+    })
+  })
+}
+
+// chnge inspiration text
